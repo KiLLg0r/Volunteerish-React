7fef1c282b6d9c139cece40af3b47e0197107b21
Robert Oblesniuc <rob.oblesniuc@gmail.com>
2022-01-15 22:01:51 +0200

    Completed front-end of form and started backend for it

--- src/components/pages/CompleteRegistration.js
+++ src/components/pages/CompleteRegistration.js
@@ -1,17 +1,29 @@
 import { useState, useRef } from "react";
+
 import { useAuth } from "../contexts/AuthContext";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import { app, auth } from "../../firebase";
 
 import { ReactComponent as VolunteerSvg } from "../../assets/svg/volunteer.svg";
 
 SwiperCore.use([Navigation]);
 
 const CompleteRegistration = () => {
+  const { currentUser } = useAuth();
+
+  const placeholderSRC =
+    "https://firebasestorage.googleapis.com/v0/b/volunteerish-ed549.appspot.com/o/placeholder.jpg?alt=media&token=8960960f-36a2-4a20-8115-c692d95e9fda";
+
   const [error, setError] = useState("");
   const [lastSlide, setLastSlide] = useState(false);
+  let file = 0;
 
   const swiperRef = useRef(null);
   const navigationPrevRef = useRef(null);
@@ -20,14 +32,16 @@ const CompleteRegistration = () => {
   const fileRef = useRef(null);
   const completeRegistrationRef = useRef(null);
 
+  const nameRef = useRef(null);
+
   const loadFile = (event) => {
     if (event.target.files && event.target.files[0]) {
-      imgRef.current.src = URL.createObjectURL(event.target.files[0]);
+      file = event.target.files[0];
+      imgRef.current.src = URL.createObjectURL(file);
     }
   };
 
   const uploadImg = () => {
-    console.log(fileRef.current);
     fileRef.current.click();
   };
 
@@ -37,54 +51,174 @@ const CompleteRegistration = () => {
 
   const FirstSlide = () => {
     return (
-      <>
+      <div className="first-slide">
         <h1 className="title">Welcome to</h1>
         <VolunteerSvg className="volunteerSvg" />
         <h1 className="title logo">Volunteerish</h1>
         <h3 className="subtitle-primary">In order to complete registration we need more information about you</h3>
-      </>
+      </div>
     );
   };
 
   const SecondSlide = () => {
     return (
-      <div className="first-slide">
-        <div className="wrapper">
-          <div className="img-upload">
-            <label htmlFor="file" style={{ cursor: "pointer" }} onClick={uploadImg} className="img-label">
-              Upload a picture
-            </label>
-            <img
-              src="https://firebasestorage.googleapis.com/v0/b/volunteerish-ed549.appspot.com/o/placeholder.jpg?alt=media&token=8960960f-36a2-4a20-8115-c692d95e9fda"
-              ref={imgRef}
-              onClick={uploadImg}
-              alt="Placeholder for uploading profile"
-              className="img-placeholder"
-            />
-            <input
-              type="file"
-              name="imageUpload"
-              onChange={loadFile}
-              style={{ display: "none" }}
-              ref={fileRef}
-              accept="image/*"
-            />
-          </div>
-          <div className="f-wrapper">
-            <input type="email" spellCheck="false" required />
-            <div className="label">Name</div>
-            <div className="icon"></div>
-          </div>
+      <div className="wrapper second-slide">
+        <div className="title">First, let's set your profile image</div>
+        <div className="img-upload">
+          <label htmlFor="file" style={{ cursor: "pointer" }} onClick={uploadImg} className="img-label">
+            Upload a picture
+          </label>
+          <img
+            src={placeholderSRC}
+            ref={imgRef}
+            onClick={uploadImg}
+            alt="Placeholder for uploading profile"
+            className="img-placeholder"
+          />
+          <input
+            type="file"
+            name="imageUpload"
+            onChange={loadFile}
+            style={{ display: "none" }}
+            ref={fileRef}
+            accept="image/*"
+          />
         </div>
       </div>
     );
   };
 
-  const handleSubmit = () => {
-    return null;
+  const ThirdSlide = () => {
+    return (
+      <div className="wrapper third-slide">
+        <div className="title">Let's add your personal information</div>
+        <div className="f-wrapper">
+          <input type="email" spellCheck="false" required ref={nameRef} />
+          <div className="label">Name</div>
+          <div className="icon"></div>
+        </div>
+        <div className="f-wrapper">
+          <input type="date" spellCheck="false" required style={{ textIndent: "5.75rem" }} />
+          <div className="label">Birth date</div>
+          <div className="icon"></div>
+        </div>
+      </div>
+    );
   };
 
-  const updateNameAndImg = () => {};
+  const FourthSlide = () => {
+    return (
+      <div className="wrapper fourth-slide">
+        <div className="title">Next, we need your address</div>
+        <div className="f-wrapper">
+          <input type="text" spellCheck="false" required style={{ textIndent: "5rem" }} />
+          <div className="label">Country</div>
+          <div className="icon"></div>
+        </div>
+        <div className="f-wrapper">
+          <input type="text" spellCheck="false" required style={{ textIndent: "3.75rem" }} />
+          <div className="label">State</div>
+          <div className="icon"></div>
+        </div>
+        <div className="f-wrapper">
+          <input type="text" spellCheck="false" required style={{ textIndent: "3.25rem" }} />
+          <div className="label">City</div>
+          <div className="icon"></div>
+        </div>
+      </div>
+    );
+  };
+
+  const FifthSlide = () => {
+    return (
+      <div className="wrapper fifth-slide">
+        <div className="title">Next, we need your address</div>
+        <div className="f-wrapper">
+          <input type="text" spellCheck="false" required style={{ textIndent: "4rem" }} />
+          <div className="label">Street</div>
+          <div className="icon"></div>
+        </div>
+        <div className="group">
+          <div className="f-wrapper">
+            <input type="number" spellCheck="false" required style={{ textIndent: "5rem" }} />
+            <div className="label">Number</div>
+            <div className="icon"></div>
+          </div>
+          <div className="f-wrapper">
+            <input type="text" spellCheck="false" required style={{ textIndent: "5rem" }} />
+            <div className="label">Building</div>
+            <div className="icon"></div>
+          </div>
+        </div>
+        <div className="f-wrapper">
+          <input type="number" spellCheck="false" required style={{ textIndent: "6.25rem" }} />
+          <div className="label">Apartment</div>
+          <div className="icon"></div>
+        </div>
+        <div className="f-wrapper" style={{ marginBlock: "1rem" }}>
+          <input type="number" spellCheck="false" required style={{ textIndent: "6rem" }} />
+          <div className="label">Post code</div>
+          <div className="icon"></div>
+        </div>
+      </div>
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const updateImg = () => {
+    if (file !== 0) {
+      const storage = firebase.app().storage("gs://volunteerish-ed549.appspot.com");
+      const profileImagesRef = storage.ref().child(`profile/${currentUser.uid}-${file.name}`);
+      const task = profileImagesRef.put(file);
+      task.on(
+        "state_changed",
+        function progress(snapshot) {},
+        function error(err) {},
+        function complete() {
+          profileImagesRef
+            .getDownloadURL()
+            .then((url) => {
+              const imageURL = url;
+              currentUser
+                .updateProfile({ photoURL: imageURL })
+                .then(() => {
+                  console.log("Successfully update image");
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+      );
+    } else {
+      currentUser
+        .updateProfile({ photoURL: placeholderSRC })
+        .then(() => {
+          console.log("Successfully update image");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+  const updateNameAndPhone = () => {
+    currentUser
+      .updateProfile({
+        displayName: nameRef.current.value,
+      })
+      .then(() => {
+        console.log("Successfully update name and phone");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   // TODO When switching up slide 0, will update profile picture and name
   // TODO Create handle submit
@@ -93,7 +227,7 @@ const CompleteRegistration = () => {
     <>
       <form action="" className="completeRegistration" ref={completeRegistrationRef}>
         <div className="close" onClick={hidePopup}>
-          X
+          &#9747;
         </div>
 
         <Swiper
@@ -108,14 +242,18 @@ const CompleteRegistration = () => {
             swiper.params.navigation.prevEl = navigationPrevRef.current;
             swiper.params.navigation.nextEl = navigationNextRef.current;
           }}
-          onInit={(swiper) => {
-            const swiperIndex = swiper.activeIndex;
-            console.log("Swiper slide changed to: " + swiperIndex);
-            console.log(swiper.slides[swiperIndex]);
-          }}
           onSlideChange={(swiper) => {
             const swiperIndex = swiper.activeIndex;
-            if (swiperIndex === 1) updateNameAndImg();
+            switch (swiperIndex) {
+              case 2:
+                updateImg();
+                break;
+              case 3:
+                updateNameAndPhone();
+                break;
+              default:
+                break;
+            }
             console.log("Swiper slide changed to: " + swiperIndex);
             console.log(swiper.slides[swiperIndex]);
           }}
@@ -132,18 +270,25 @@ const CompleteRegistration = () => {
           <SwiperSlide>
             <SecondSlide />
           </SwiperSlide>
-          <SwiperSlide>3</SwiperSlide>
-          <SwiperSlide>4</SwiperSlide>
-          <SwiperSlide>5</SwiperSlide>
-          <SwiperSlide>6</SwiperSlide>
-          <div className="prev-btn" ref={navigationPrevRef}>
-            Previous
-          </div>
-          <div className="next-btn" ref={navigationNextRef}>
-            Next
-          </div>
-          <div className={`complete-btn ${lastSlide ? "last-slide" : "not-last-slide"}`} onClick={handleSubmit}>
-            Complete registration
+          <SwiperSlide>
+            <ThirdSlide />
+          </SwiperSlide>
+          <SwiperSlide>
+            <FourthSlide />
+          </SwiperSlide>
+          <SwiperSlide>
+            <FifthSlide />
+          </SwiperSlide>
+          <div className="btns">
+            <div className="prev-btn" ref={navigationPrevRef}>
+              Previous
+            </div>
+            <div className="next-btn" ref={navigationNextRef}>
+              Next
+            </div>
+            <div className={`complete-btn ${lastSlide ? "last-slide" : "not-last-slide"}`} onClick={handleSubmit}>
+              Complete registration
+            </div>
           </div>
         </Swiper>
       </form>
