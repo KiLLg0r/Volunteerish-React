9567f6ea8875ae922ec969b86bb21734f10cb60c
Robert Oblesniuc <rob.oblesniuc@gmail.com>
2022-01-15 15:55:58 +0200

    Stored Firebase API keys to local env

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyB3YHpgOulmWy1KkuPgB3XtfdFu4Q-iDCY",
-  authDomain: "volunteerish-ed549.firebaseapp.com",
-  projectId: "volunteerish-ed549",
-  storageBucket: "volunteerish-ed549.appspot.com",
-  messagingSenderId: "970687537120",
-  appId: "1:970687537120:web:2d0d2bf109195bbbcc9585",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export const auth = app.auth();
